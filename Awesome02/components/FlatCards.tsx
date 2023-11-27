fc88c22d1a319d54b0a9bb17c7b611e3e184d159
diegosilva-plank <diego.silva@joinplank.com>
2023-11-27 10:26:04 -0300

    cards as scrollview

--- Awesome02/components/FlatCards.tsx
+++ Awesome02/components/FlatCards.tsx
@@ -4,9 +4,52 @@ import React from 'react'
 export default function FlatCards() {
   return (
     <View>
-      <Text>FlatCards</Text>
+      <Text style={styles.headingText}>FlatCards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+            <Text style={styles.blankText}>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+            <Text style={styles.blankText}>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+            <Text style={styles.blankText}>Blue</Text>
+        </View>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8,
+    },
+    blankText: {
+        color: '#FFFFFF',
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 8,
+    },
+    card: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 100,
+        height: 100,
+        borderRadius: 4,
+        margin: 8,
+    },
+    cardOne: {
+        backgroundColor: '#EF5354',
+    },
+    cardTwo: {
+        backgroundColor: '#50DBB4',
+    },
+    cardThree: {
+        backgroundColor: '#5DA3FA',
+    },
+})
