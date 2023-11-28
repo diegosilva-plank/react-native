b07e657c4528af897df71937924aab39c2b0bfe7
diegosilva-plank <diego.silva@joinplank.com>
2023-11-28 09:58:16 -0300

    card styling

--- Awesome02/components/FancyCard.tsx
+++ Awesome02/components/FancyCard.tsx
@@ -6,11 +6,15 @@ export default function FancyCard() {
     <View>
       <Text style={styles.headingText}>Trending places</Text>
       <View style={[styles.card, styles.cardElevated]}>
-      <Image 
-        source={{
-            uri: 'https://i0.wp.com/media.architecturaldigest.com/photos/5dcc33a8995edd00090eeb8e/16:9/w_4287,h_2411,c_limit/GettyImages-158630535.jpg?ssl=1'
-        }}
-        style={styles.cardImage} />
+        <Image 
+          source={{
+            uri: 'https://i0.wp.com/media.architecturaldigest.com/photos/5dcc33a8995edd00090eeb8e/16:9/w_4287,h_2411,c_limit/GettyImages-158630535.jpg?ssl=1',
+            headers: {
+              Accept: '*/*',
+            },
+          }}
+          style={styles.cardImage} 
+        />
         <View style={styles.cardBody}>
             <Text style={styles.cardTitle}>Hawa Mahal</Text>
             <Text style={styles.cardLabel}>Pink City, Jaipur</Text>
@@ -25,15 +29,41 @@ export default function FancyCard() {
 }
 
 const styles = StyleSheet.create({
-    headingText: {},
-    card: {},
-    cardElevated: {},
+    headingText: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      paddingHorizontal: 8,
+    },
+    card: {
+      height: 360,
+      borderRadius: 6,
+      marginVertical: 12,
+      marginHorizontal: 16,
+    },
+    cardElevated: {
+      backgroundColor: '#DDDDDD',
+      elevation: 3,
+    },
     cardImage: {
         height: 180,
+        borderTopLeftRadius: 6,
+        borderTopRightRadius: 6,
     },
-    cardBody: {},
-    cardTitle: {},
-    cardLabel: {},
-    cardDescription: {},
+    cardBody: {
+      flex: 1,
+      flexGrow: 1,
+      paddingVertical: 12,
+      paddingHorizontal: 12,
+    },
+    cardTitle: {
+      color: '#000000',
+
+    },
+    cardLabel: {
+      color: '#000000',
+    },
+    cardDescription: {
+      color: '#000000',
+    },
     cardFooter: {},
 })
