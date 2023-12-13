7cd2b32e1fca90a4a2e58b2f2f38e0f5b60a6164
diegosilva-plank <diego.silva@joinplank.com>
2023-12-13 14:46:06 -0300

    currencyConverter06

--- currencyConverter06/App.tsx
+++ currencyConverter06/App.tsx
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Pressable, FlatList, NativeModules } from 'react-native';
 
 // Constants
 import { currencyByRupee } from './src/constants';
 // Components
 import CurrencyButton from './src/components/CurrencyButton';
 
-import Snackbar from 'react-native-snackbar';
 import { useState } from 'react';
 
 export default function App() {
@@ -16,7 +15,7 @@ export default function App() {
 
   const buttonPressed = (targetValue: Currency) => {
     if (!inputValue) {
-      return Snackbar.show({
+      return NativeModules.RNSnackbar.show({
         text: 'Enter a value to convert',
         backgroundColor: '#EA7773',
         textColor: '#000000'
@@ -30,7 +29,7 @@ export default function App() {
       setResultValue(result)
       setTargetCurrency(targetValue.name)
     } else {
-      return Snackbar.show({
+      return NativeModules.RNSnackbar.show({
         text: 'Not a valid number to convert',
         backgroundColor: '#F4BE2C',
         textColor: '#000000'
@@ -39,9 +38,106 @@ export default function App() {
   }
 
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar/>
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <View style={styles.rupeesContainer}>
+            <Text style={styles.rupee}>₹</Text>
+            <TextInput
+            maxLength={14}
+            value={inputValue}
+            clearButtonMode='always' //only for iOS
+            onChangeText={setInputValue}
+            keyboardType='number-pad'
+            placeholder='Enter amount in Rupees'
+            />
+          </View>
+          {resultValue && (
+            <Text style={styles.resultTxt} >
+              {resultValue}
+            </Text>
+          )}
+        </View>
+        <View style={styles.bottomContainer}>
+          <FlatList
+          numColumns={3}
+          data={currencyByRupee}
+          keyExtractor={item => item.name}
+          renderItem={({item}) => (
+            <Pressable
+            style={[
+              styles.button, 
+              targetCurrency === item.name && styles.selected
+            ]}
+            onPress={() => buttonPressed(item)}
+            >
+              <CurrencyButton {...item} />
+            </Pressable>
+          )}
+          />
+        </View>
+      </View>
+      
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#c3c3c3',
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  resultTxt: {
+    fontSize: 32,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupee: {
+    marginRight: 8,
+
+    fontSize: 22,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupeesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputAmountField: {
+    height: 40,
+    width: 200,
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 4,
+    backgroundColor: '#FFFFFF',
+  },
+  bottomContainer: {
+    flex: 3,
+  },
+  button: {
+    flex: 1,
+
+    margin: 12,
+    height: 60,
+
+    borderRadius: 12,
+    backgroundColor: '#fff',
+    elevation: 2,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+  },
+  selected: {
+    backgroundColor: '#ffeaa7',
+  },
+});
--- currencyConverter06/src/components/CurrencyButton.tsx
+++ currencyConverter06/src/components/CurrencyButton.tsx
@@ -5,13 +5,13 @@ import { View, Text, StyleSheet } from 'react-native'
 
 type CurrencyButtonProps = PropsWithChildren<{
     name: string;
-    falg: string;
+    flag: string;
 }>
 
 const CurrencyButton = (props: CurrencyButtonProps): JSX.Element => {
     return (
         <View style={styles.buttonContainer}>
-            <Text style={styles.flag}>{props.falg}</Text>
+            <Text style={styles.flag}>{props.flag}</Text>
             <Text style={styles.country}>{props.name}</Text>
         </View>
     )
