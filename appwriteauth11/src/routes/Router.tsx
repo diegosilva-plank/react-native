898cb99a30a450082e50dbc3564423447260fe5f
diegosilva-plank <diego.silva@joinplank.com>
2024-03-18 16:24:08 -0300

    finishing appwriteauth11

---
+++ appwriteauth11/src/routes/Router.tsx
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+import { AppwriteContext } from '../appwrite/AppwriteContext'
+import Loading from '../components/Loading'
+
+//Routes
+import { AuthStack } from './AuthStack'
+import { AppStack } from './AppStack'
+
+export const Router = () => {
+  const  [isLoading, setIsLoading] = useState<boolean>(true)
+  const {appwrite, isLoggedIn, setIsLoggedIn} = useContext(AppwriteContext)
+
+  useEffect(() => {
+    appwrite
+      .getCurrentUser()
+      .then(response => {
+        setIsLoggedIn(false)
+        if (response) {
+          setIsLoggedIn(true)
+        }
+      })
+      .catch(_ => {
+        setIsLoading(false)
+        setIsLoggedIn(false)
+      })
+  }, [appwrite, setIsLoggedIn])
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  return (
+    <NavigationContainer>
+      {isLoggedIn ? <AppStack /> : <AuthStack />}
+    </NavigationContainer>
+  )
+}
